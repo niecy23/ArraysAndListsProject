56279987ac995d1aff55d778e68ae108dda71fde
Chanise Hart <niecy23@gmail.com>
2022-12-20 18:16:06 -0700

    Exercise 2 without Video

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -8,25 +9,81 @@ namespace ArraysAndLists
         {
             //TODO:
 
-            // Create an int Array and populate numbers 1-10
+            // Create an int Array and populate numbers 1-10 - DONE
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
+            var numbers = new int[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+
+            /* Create two Lists of type int. - DONE
+             * Name one List "evens" - DONE
+             * Name the other List "odds" - DONE
              */
 
-            /* Using either a foreach or for loop,
+            var evens = new List<int>();
+            var odds = new List<int>();
+
+            /* Using either a foreach or for loop, - DONE
              * nest an if statement to check to see
-             *  if a number is even or odd.
+             *  if a number is even or odd. - DONE
              * Then add those numbers to either the evens List
-             * or the odds List
+             * or the odds List - DONE
              */
+
+            foreach (var num in numbers)
+                {
+                    if (num % 2 == 0)
+                    {
+                        evens.Add(num);
+                    }
+                    else
+                    {
+                        odds.Add(num);
+                    }
+                }
 
             /* Now using foreach or for loops,
-             * display each List of even and odd numbers
+             * display each List of even and odd numbers - DONE
              *
              * Try to be creative in your display
              */
+
+            Console.WriteLine("True or False: I love even numbers!");
+            var evenOrOdd = bool.Parse(Console.ReadLine());
+
+            if (evenOrOdd == true)
+                {
+                    Console.WriteLine("Even it is!");
+
+                    foreach (var num in evens)
+                    {
+                        Console.WriteLine(num);  
+                    }
+
+                    Console.WriteLine("But we cannot forget about the odd numbers, so here they are.");
+
+                    foreach (var num in odds)
+                    {
+                        Console.WriteLine(num);
+                    }
+            }
+            else
+                {
+                    Console.WriteLine("Odd it is!");
+
+                    foreach (var num in odds)
+                    {
+                        Console.WriteLine(num);
+                    }
+
+                    Console.WriteLine("But we cannot forget about the even numbers, so here they are.");
+
+                    foreach (var num in evens)
+                    {
+                        Console.WriteLine(num);
+                    }
+            }
+
         }
     }
 }
